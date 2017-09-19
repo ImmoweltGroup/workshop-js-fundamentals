595144caa1b0367ec2951498bf3a4192e1701724
Tyll Weiß <tyll.weiss.extern@axelspringer.com>
2017-09-19 09:40:04 +0200

    FEATURE: Enhance the flow type examples by defining more complex data types

--- src/flow/index.js
+++ src/flow/index.js
@@ -9,3 +9,23 @@ const multiplyOptional = (subject?: number = 0): number => subject * subject;
 multiplyOptional(); // Works
 multiplyOptional(2); // Works
 multiplyOptional('Foo'); // Error
+
+//
+// Declaring re-usable types is also possible, by prepending `export` in front of `type` you can
+//
+type MyArgType = string;
+
+const concatenate = (subject: MyArgType) => subject + subject;
+
+concatenate('Foo'); // Works
+concatenate(2); // Error
+
+//
+// Exporting / Importing types is possible as well, for demonstration purposes the import is within the file,
+// usually they should be at the top of the file so developers can easily scan all imported types / dependencies.
+//
+import type {ArrayOfNumbers, ObjectWithNumbers} from './types.js';
+
+const foo = (foo: ArrayOfNumbers, bar: ObjectWithNumbers) => 2;
+
+foo([2], {foo: 2}); // Works
---
+++ src/flow/types.js
@@ -0,0 +1,6 @@
+// @flow
+
+export type ArrayOfNumbers = Array<number>;
+export type ObjectWithNumbers = {
+  [string]: number
+};
