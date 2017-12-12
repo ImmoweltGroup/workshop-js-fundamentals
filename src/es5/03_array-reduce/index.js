10ffe6c78f05a09ce739d6a27ab8b96e6e7f559e
Kevin Ruff <kevin.ruff@immonet.de>
2017-12-12 10:07:18 +0100

    Remove es6 features in es5 section

--- src/es5/03_array-reduce/index.js
+++ src/es5/03_array-reduce/index.js
@@ -2,10 +2,10 @@
 // Using the `.reduce(fn, initialValue)` method of the built in Array Constructor adds more flexability to the finalized value.
 // In this example we filter out duplicate id's.
 //
-const objects = [{id: 0}, {id: 2}, {id: 6}, {id: 2}];
-const initialValue = [];
-const objectIds = objects.reduce((value, obj) => {
-  const id = obj.id;
+var objects = [{id: 0}, {id: 2}, {id: 6}, {id: 2}];
+var initialValue = [];
+var objectIds = objects.reduce(function (value, obj) {
+  var id = obj.id;
 
   if (value.includes(id) === false) {
     value.push(id);
